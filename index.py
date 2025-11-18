6ab0ed987bb0ecd90cb8c64b2ae0f552c6be9389
amjadajuz <ptamjadaju@gmail.com>
2025-11-18 20:57:47 +0530

    Implement initial version of a helpful assistant agent using Google ADK, including configuration for Google API and retry options. The agent is designed to answer general questions and utilize Google Search for up-to-date information.

---
+++ index.py
@@ -0,0 +1,39 @@
+import os
+import asyncio
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+from google.genai import types
+
+GOOGLE_API_KEY = "AIzaSyCwnyYWonWTPjJPmDByVhKeLqG6Kl4FVBw"
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,
+    exp_base=7,
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504]
+)
+
+root_agent = Agent(
+    name="helpful_assistant",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    description="A simple agent that can answer general questions.",
+    instruction="You are a helpful assistant. Use Google Search for current info or if unsure.",
+    tools=[google_search],
+)
+
+runner = InMemoryRunner(agent=root_agent)
+
+async def main():
+    response = await runner.run_debug(
+        "What is Agent Development Kit from Google? What languages is the SDK available in?"
+    )
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
