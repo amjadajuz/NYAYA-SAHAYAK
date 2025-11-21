04519239ca592407690fcc559ff3a368f24edd48
amjadajuz <ptamjadaju@gmail.com>
2025-11-21 10:59:27 +0530

    Add data collection and research assistant agents with specific instructions and configurations

--- index.py
+++ index.py
@@ -16,6 +16,28 @@ retry_config = types.HttpRetryOptions(
     http_status_codes=[429, 500, 503, 504]
 )
 
+data_collection_agent = Agent(
+    name="data_collection_assistant",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    description="Agent to ensure all data is given from user request.",
+    instruction="You need to ensure all data is given from user request. To give proper law points we need to get a proper problem statement from user request such as incident date, time, location, victims, witnesses, and other relevant details. You can use Google Search to  know about the case requirements.",
+    tools=[google_search],
+)
+
+research_agent = Agent(
+    name="research_assistant",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    description="A research assistant that can research law points.",
+    instruction="You are a research assistant.You will be given a question and you will need to research the law points and provide the answer. Use Google Search for all questions and prioritize the latest information.",
+    tools=[google_search],
+)
+
 root_agent = Agent(
     name="helpful_assistant",
     model=Gemini(
